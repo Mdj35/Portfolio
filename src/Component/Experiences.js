fdb31ee514147e55a97ef5a3749f107ced7c8b26
Mdj35 <vynceian.oani@hcdc.edu.ph>
2026-03-18 16:41:29 +0800

    sample

--- src/Component/Experiences.js
+++ src/Component/Experiences.js
@@ -7,6 +7,14 @@ const experiences = [
     description: "Part-Time Frontend Developer for the development of the HCDC Website using HTML, CSS and Javascript ensuring a responsive and user-friendly interface.",
     skills: ["HTML", "JavaScript", "CSS"],
   },
+  {
+    id: 2,
+    company: "Department of Social Welfare and Development (DSWD)",
+    position: "Fullstack Developer - Intern",
+    duration: "February 2025 - Present",
+    description: "Fullstack Developer Intern responsible for building and maintaining web applications using Laravel backend and React frontend, with MySQL database management. Contributed to developing features and improving application performance.",
+    skills: ["Laravel", "MySQL", "React"],
+  },
  
   // Add more experiences as needed
 ];
